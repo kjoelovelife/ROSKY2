5d0841e25b73d325bb35ae7fcf44f7ee780dafea
Wei-Chih Lin <weichih.lin@protonmail.com>
2022-07-07 15:30:21 +0800

    Fix: What the environment using now
    Problem:
      1. We could not know what the operating environment after
         loading ros_menu.
    
    Reason:
      1. No information on the terminal.
    
    Adjust item:
      1. Add the command to show on the screen.

--- setup/shell_scripts/environment.sh
+++ setup/shell_scripts/environment.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-
 # get shell on this system
 shell=$(echo $SHELL | awk -F '/' '{print $NF}')
 
+# show information
+echo -e "\nSetup operating environment for ROSKY2"
+
+
 # source ros2_ws/install/setup.bash
-source ${HOME}/ROSKY2/ros2_ws/install/local_setup.${shell}
+source ${HOME}/ROSKY2/ros2_ws/install/setup.${shell}
 
 # set vehicle name
 source ${HOME}/ROSKY2/setup/shell_scripts/set_vehicle_name.sh
+
+# show information
+echo -e "Now you can start using ROSKY2.\n"
