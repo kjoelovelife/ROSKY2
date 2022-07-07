6daa2c287bb19166402d035b7470cdd1c4e74e44
Wei-Chih Lin <weichih.lin@protonmail.com>
2022-07-07 15:23:03 +0800

    Fix: Launch the file rosky2_bringup.launch.py
    Problem:
      1. Failed to run the node in package "ominibot_car"
    
    Reason:
      1. Vechile_name could not use symbol "-" but $HOSTNAME did.
    
    Adjust item:
      1. Mandatory to change the symbol "-" to "_" from $HOSTNAME,
         and then assigned to the variable $VEHICLE_NAME in line 7.

--- setup/shell_scripts/set_vehicle_name.sh
+++ setup/shell_scripts/set_vehicle_name.sh
@@ -4,7 +4,7 @@ if [ $# -gt 0 ]; then
 	# provided a hostname, use it as ROS_MASTER_URI
 	export VEHICLE_NAME=$1
 else
-	echo "No hostname provided. Using $HOSTNAME."
-	export VEHICLE_NAME=$HOSTNAME
+	export VEHICLE_NAME=$(echo $HOSTNAME | sed -e "s:\-:_:g")
+	echo "No hostname provided. Using $VEHICLE_NAME."
 fi
 echo "VEHICLE_NAME set to $VEHICLE_NAME"
