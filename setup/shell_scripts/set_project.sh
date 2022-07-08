ab265d5dcf85511506c227d2ba614168df0b965d
Wei-Chih Lin <weichih.lin@protonmail.com>
2022-07-08 10:37:53 +0800

    Add a new script to setup the operating environment for this project

--- setup/shell_scripts/set_project.sh
+++ setup/shell_scripts/set_project.sh
@@ -2,12 +2,12 @@
 #
 # Copyright (c) 2021 Wei-Chih Lin(weichih.lin@protonmail.com)
 #
-#   This file will help you setup the environment for ROSKY2 
-#   Support platform:
-#     1. reComputer J1010 with Jetpack 4.6 
-#        (https://www.icshop.com.tw/product-page.php?28703)
-#     2. Jetson Nano Developer kit for third party Ubuntu 20.04 with Jetpack 4.3
-#        (https://forums.developer.nvidia.com/t/xubuntu-20-04-focal-fossa-l4t-r32-3-1-custom-image-for-the-jetson-nano/121768)
+# This file will help you setup the environment for ROSKY2 
+# Support platform:
+#   1. reComputer J1010 with Jetpack 4.6 
+#      (https://www.icshop.com.tw/product-page.php?28703)
+#   2. Jetson Nano Developer kit for third party Ubuntu 20.04 with Jetpack 4.3
+#      (https://forums.developer.nvidia.com/t/xubuntu-20-04-focal-fossa-l4t-r32-3-1-custom-image-for-the-jetson-nano/121768)
 #
 
 
